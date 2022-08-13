5614065d287ea8a7b89581196caa2d9df79dba20
Rowland <cjrowland4@gmail.com>
2022-08-13 11:06:45 -0500

    Added a function to test the efficiency of solutions against each other.

---
+++ performance_test.py
@@ -0,0 +1,17 @@
+import time
+
+
+
+def performance_test(func1, func2, arg):
+    start = time.perf_counter()
+    func1(arg)
+    print(f"{func1.__name__} time: {time.perf_counter() - start}")
+
+    start = time.perf_counter()
+    func2(arg)
+    print(f"{func2.__name__} time: {time.perf_counter() - start}")
+
+    return
+
+
+performance_test(Easy.L)

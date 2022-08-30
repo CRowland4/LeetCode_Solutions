071fdc4f77f4668f4c85df50f445cb4cca8fec21
Caleb Rowland <cjrowland4@gmail.com>
2022-08-29 20:50:00 -0500

    Added a Reusable Functions directory and started it off with a function to find prime numbers.

---
+++ Reusable_Functions/prime_finder.py
@@ -0,0 +1,14 @@
+import math
+
+
+def prime_finder(upper_limit):
+    primes = [2, 3, 5, 7, 11]
+
+    for number in range(13, upper_limit + 1, 2):
+        if all([number % prime != 0 for prime in primes if prime < math.floor(math.sqrt(number)) + 1]):
+            primes.append(number)
+            print(number)
+
+    return primes
+
+
